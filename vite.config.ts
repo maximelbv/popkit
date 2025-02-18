e35f91af038010dc0d94e2bef8980606ee88710a
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-02-18 09:56:38 +0100

    set up chakra ui and alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@content": path.resolve(__dirname, "src/content"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+});
