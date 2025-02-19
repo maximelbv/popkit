5417f39ef94f34b937fab777f733a46f48e72f13
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-02-19 13:41:37 +0100

    stylize sidebar tags

--- src/components/SidebarTag.tsx
+++ src/components/SidebarTag.tsx
@@ -4,8 +4,14 @@ interface ISidebarTagProps {
 
 const SidebarTag = ({ type }: ISidebarTagProps) => {
   return (
-    <div>
-      <span className="!text-[14px]">{type}</span>
+    <div
+      className={`flex items-center justify-center !px-1.5 !font-medium rounded-md !border !border-solid ${
+        type === "Updated"
+          ? "bg-purple-400/20 text-purple-400 !border-purple-400/50"
+          : "bg-blue-400/20 text-blue-400 !border-blue-400/50"
+      }`}
+    >
+      <span className="!text-[12px]">{type}</span>
     </div>
   );
 };
