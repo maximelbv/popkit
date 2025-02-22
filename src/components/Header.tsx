585e4064ca935b50e2546b116f52910a1d1519b0
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-02-22 11:48:21 +0100

    add final logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { IconButton } from "@chakra-ui/react";
 import { BsGithub } from "react-icons/bs";
 import Logo from "./Logo";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
     <div className="flex items-center justify-between w-full max-w-layout-max-w !m-auto !p-4">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <div>
         <IconButton aria-label="github" variant="outline" asChild>
           <a href="https://github.com/maximelbv/popkit" target="blank_">
