4a2c98e762c65a9dbd22bd5289acb13d4d5358d8
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-03-01 14:13:28 +0100

    remove unused type

--- src/types/dependencies.ts
+++ src/types/dependencies.ts
@@ -1,8 +1,4 @@
-import { DependenciesList } from "@/constants/dependencies";
-
 export interface IDependency {
   name: string;
   link: string;
 }
-
-export type Dependency = (typeof DependenciesList)[number]["name"];
