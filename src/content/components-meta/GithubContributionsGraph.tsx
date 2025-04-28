a62600c54a69a2d380c11765107996a950791883
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-04-28 16:48:36 +0200

    create meta template fot the contrib graph

---
+++ src/content/components-meta/GithubContributionsGraph.tsx
@@ -0,0 +1,72 @@
+import MiniPreviewWrapper from "@/components/docs/MiniPreviewWrapper";
+import PreviewWrapper from "@/components/docs/PreviewWrapper";
+import { Categories } from "@/constants/categories";
+import { Status } from "@/constants/status";
+import { IComponent } from "@/types/components";
+
+const manualInstallation = {
+  mode: "manual",
+  variantSelectable: true,
+  steps: [
+    {
+      step: 1,
+      title: "Copy the component to your project",
+      description: "Place it in your /components folder",
+      codeBlock: {
+        tsTailwind: "",
+      },
+    },
+    {
+      step: 2,
+      title: "Import and Customize",
+      description:
+        "Use it anywhere in your app, adjust props for customization",
+      codeBlock: {
+        tsTailwind: ``,
+      },
+    },
+  ],
+};
+
+const overview = {
+  miniPreview: () => (
+    <MiniPreviewWrapper>
+      <></>
+    </MiniPreviewWrapper>
+  ),
+  preview: () => (
+    <PreviewWrapper>
+      <></>
+    </PreviewWrapper>
+  ),
+  code: ``,
+  quickCopyCode: manualInstallation.steps[0].codeBlock.tsTailwind,
+};
+
+const props = [
+  {
+    property: "",
+    type: "",
+    default: "",
+    description: "",
+  },
+];
+
+const ex = {
+  examples: () => <div className="grid gap-4 grid-cols-1 md:grid-cols-2"></div>,
+  miniExamples: () => <div className="grid gap-4 grid-cols"></div>,
+};
+
+const Meta: IComponent = {
+  name: "Github Contributions Graph",
+  description: "",
+  category: Categories.DATA_VIZ,
+  status: Status.NEW,
+  overview: overview,
+  installation: [manualInstallation],
+  examples: ex,
+  props: props,
+  dependencies: [],
+};
+
+export default Meta;
